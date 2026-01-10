655247ab0002d7b2d1f55bf7e1a82919f1206796
Mohammed Abubakar <mohammedauwalnasara@gmail.com>
2026-01-10 23:55:31 +0100

    fix: corrected profile screen routing and imports for eSportlyic

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -1,4 +1,6 @@
 import "../../../core/widgets/league_switcher.dart";
+import "../../../core/widgets/league_switcher.dart";
+import "../../../core/widgets/league_switcher.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
