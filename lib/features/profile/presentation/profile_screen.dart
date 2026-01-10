5cbb8062a6ea8853cdb117bcb79f430ff06c3ad5
Mohammed Abubakar <mohammedauwalnasara@gmail.com>
2026-01-11 00:02:58 +0100

    fix: corrected project-wide presentation paths and resolved profile screen build error

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -1,6 +1,7 @@
 import "../../../core/widgets/league_switcher.dart";
 import "../../../core/widgets/league_switcher.dart";
 import "../../../core/widgets/league_switcher.dart";
+import "../../../core/widgets/league_switcher.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
